99a50cd1e2aaaa6bce89c314879b6f571dfadeac
Zayneeh <tairuzainab5@gmail.com>
2025-05-16 19:37:19 +0100

    commented out ocr reader because i havent created a function for it

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-# Ocr reader 
-LABEL_DIR = '/content/drive/MyDrive/Medivoice/labels'
-ocr_result = extract_text_from_latest_label(LABEL_DIR)
+from IPython.display import Audio
+from scripts.speech_to_text import speech_to_text
 
-# -- Run Speech-to-Text --
+# Ocr reader 
+#LABEL_DIR = '/content/drive/MyDrive/Medivoice/labels'
+#ocr_result = extract_text_from_latest_label(LABEL_DIR)
+
+#Run Speech-to-Text --
 input_path = "/content/drive/MyDrive/RECORD.m4a"
 IPyAudio(input_path)  # Optional: plays back the audio in notebook
-
-wav_path = convert_to_wav(input_path)
 transcribed_text = speech_to_text(wav_path)
 print("\n=== TRANSCRIPTION ===\n", transcribed_text)
-
-
-use 
